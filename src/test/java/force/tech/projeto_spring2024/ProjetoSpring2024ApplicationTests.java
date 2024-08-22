0ec3251a0e941aca4224fd5483c7d70aaad73835
Isabel Reis <111527484+IsabelRReis@users.noreply.github.com>
2024-08-21 23:09:17 -0300

    Add files via upload

---
+++ src/test/java/force/tech/projeto_spring2024/ProjetoSpring2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package force.tech.projeto_spring2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpring2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
